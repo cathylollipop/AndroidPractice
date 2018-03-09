557074973500e63ffcb2a9c209b87e02614cff96
xiaoli <lixiao900921@gmail.com>
2018-03-08 21:39:27 -0600

    Guess Celebrity

--- GuessCelebrity/app/src/main/java/com/practice/xiaoli/guesscelebrity/MainActivity.java
+++ GuessCelebrity/app/src/main/java/com/practice/xiaoli/guesscelebrity/MainActivity.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +18,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
@@ -26,11 +29,56 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> celebURLs = new ArrayList<String>();
     ArrayList<String> celebNames = new ArrayList<String>();
     int chosenCeleb = 0;
+    int correctAnswerLocation = 0;
+    int incorrectAnswerLocation = 0;
+    String[] answers = new String[4];
 
     ImageView imageView;
 
-    public void guessCelebrity(View view){
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
 
+    public void generateNewQuestion(){
+        Random random = new Random();
+        chosenCeleb = random.nextInt(celebURLs.size());
+
+        ImageDownloader imageTask = new ImageDownloader();
+        try{
+            Bitmap resultImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+            imageView.setImageBitmap(resultImage);
+
+            correctAnswerLocation = random.nextInt(4);
+
+            for(int i = 0; i < 4; i++){
+                incorrectAnswerLocation = random.nextInt(celebNames.size());
+                HashSet<Integer> set = new HashSet<>();
+                while(incorrectAnswerLocation == chosenCeleb || !set.add(incorrectAnswerLocation)){
+                    incorrectAnswerLocation = random.nextInt(4);
+                }
+                if(i == correctAnswerLocation)
+                    answers[i] = celebNames.get(chosenCeleb);
+                else
+                    answers[i] = celebNames.get(incorrectAnswerLocation);
+            }
+
+            button0.setText(answers[0]);
+            button1.setText(answers[1]);
+            button2.setText(answers[2]);
+            button3.setText(answers[3]);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void guessCelebrity(View view){
+        if(view.getTag().toString().equals(Integer.toString(correctAnswerLocation))){
+            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "Wrong! It was " + celebNames.get(chosenCeleb), Toast.LENGTH_LONG).show();
+        }
+        generateNewQuestion();
     }
 
     public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
@@ -95,6 +143,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = (ImageView)findViewById(R.id.imageView);
+        button0 = (Button)findViewById(R.id.button0);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
 
         DownloadTask task = new DownloadTask();
         String result = null;
@@ -116,13 +168,7 @@ public class MainActivity extends AppCompatActivity {
                 celebNames.add(m.group(1));
             }
 
-            Random random = new Random();
-            chosenCeleb = random.nextInt(celebURLs.size());
-
-            ImageDownloader imageTask = new ImageDownloader();
-            Bitmap resultImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
-
-            imageView.setImageBitmap(resultImage);
+            generateNewQuestion();
 
         } catch (InterruptedException e) {
             e.printStackTrace();
